d236a62e63c0a8a81a52775d7387a3e5f0674af3
Marian Schneider <smarian@ethz.ch>
2025-12-06 15:20:40 +0100

    add 2025 day 4 part 2

---
+++ 2025/Day_04/part_2.py
@@ -0,0 +1,50 @@
+INPUT_PATH = "2025/Day_04/input.txt"
+
+OFFSETS = [(-1, -1), (-1,  0), (-1,  1),
+           ( 0, -1),           ( 0,  1),
+           ( 1, -1), ( 1,  0), ( 1,  1)]
+
+if __name__ == "__main__":
+    # === READ AND PARSE INPUT ===
+    with open(INPUT_PATH, "r") as file:
+        lines = file.readlines()
+
+    grid = [[cell == "@" for cell in line.strip()] for line in lines]
+
+    # === SOLVE ===
+    result = 0
+    prev_result = -1
+
+    num_rows = len(grid)
+    num_cols = len(grid[0])
+
+    while prev_result != result:
+        prev_result = result
+        next_grid = [[False for _ in range(num_cols)] for _ in range(num_rows)] 
+
+        for r in range(num_rows):
+            for c in range(num_cols):
+                if not grid[r][c]:
+                    continue
+
+                num_adjacent = 0
+
+                for dr, dc in OFFSETS:
+                    updated_r = r + dr
+                    updated_c = c + dc
+
+                    if not (0 <= updated_r < num_rows) or not (0 <= updated_c < num_cols):
+                        continue
+                    
+                    if grid[updated_r][updated_c]:
+                        num_adjacent += 1
+
+                if num_adjacent < 4:
+                    result += 1
+                else:
+                    next_grid[r][c] = True
+
+        grid = next_grid
+
+    # === OUTPUT ===
+    print("Number of accessible rolls of paper:", result)
